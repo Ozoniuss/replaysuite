f98ba740ec928c8802237f39f8b354826e3bf68d
Ozoniuss <qscxzsewqazxcde@gmail.com>
2026-05-25 19:13:14 +0300

    Set maximum attempts to 1 in retry policy of the interceptor

--- interceptor.go
+++ interceptor.go
@@ -47,18 +47,22 @@ func (o *fastReplayOutbound) ExecuteActivity(
 ) workflow.Future {
 	opts := workflow.GetActivityOptions(ctx)
 	opts.TaskQueue = sharedTaskQueue
-	// This is intended. Testing an activity with an infinite retry
-	// policy and a forced failure on that activity is probably a
-	// wrong test.
-	//
-	// TODO: have a test for infinite retry policy to see what happens.
-	// Don't want to have the server running forever.
+	// Overwrite the retry policy to something that executes quickly on the
+	// dev server.
 	if opts.RetryPolicy == nil {
 		opts.RetryPolicy = &temporal.RetryPolicy{}
 	}
 	opts.RetryPolicy.InitialInterval = shortRetryInterval
 	opts.RetryPolicy.MaximumInterval = shortRetryInterval
 	opts.RetryPolicy.BackoffCoefficient = 1.0
+	// It currently seems that even though the retry interval is below 1 second,
+	// the activity retries are still attempted once every second on the dev
+	// server. Disabling retries until I figure out if this is actually a
+	// limitation.
+	//
+	// TODO: this overwrites infinite retries. need to figure out exactly what
+	// to do there.
+	opts.RetryPolicy.MaximumAttempts = 1
 	ctx = workflow.WithActivityOptions(ctx, opts)
 	return o.Next.ExecuteActivity(ctx, activityType, args...)
 }
